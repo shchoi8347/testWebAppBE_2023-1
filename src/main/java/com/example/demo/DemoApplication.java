e6d770706e2d4e70bad5afe37e04226ee2411247
shchoi8347 <csh@duksung.ac.kr>
2023-03-09 13:58:08 +0900

    02

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// 01
+		// 01 -> 02
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
